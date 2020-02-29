0302ebea2d13819420737225eb3eaa8f27d9781c
Rhys Campbell <rhys.james.campbell@googlemail.com>
2020-02-29 12:19:30 +0100

    Remove host file test

--- roles/cassandra_dba/molecule/default/tests/test_default.py
+++ roles/cassandra_dba/molecule/default/tests/test_default.py
@@ -7,14 +7,6 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 ).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
-
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
-
-
 def test_packages_installed(host):
     # TODO Include ansible vars from file
 
