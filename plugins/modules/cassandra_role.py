441e8d4311712a7bb1488eaee06b3202c5bd0793
Rhys Campbell <rhys.james.campbell@googlemail.com>
2020-03-26 08:08:10 +0100

    cd into user home

--- plugins/modules/cassandra_role.py
+++ plugins/modules/cassandra_role.py
@@ -749,12 +749,7 @@ def main():
         module.exit_json(**result)
 
     except Exception as excep:
-        exType, ex, tb = sys.exc_info()
-        msg = "An error occured on line {0}: {1} | {2} | {3} | {4}".format(traceback.tb_lineno(tb),
-                                                                           excep,
-                                                                           exType,
-                                                                           ex,
-                                                                           traceback.print_exc())
+        msg = excep
         if cql is not None:
             msg += " | {0}".format(cql)
         if debug:
