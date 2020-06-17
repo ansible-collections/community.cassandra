4a4a30ecbf1a1dc84ef59aca6db71c87b72abf8f
Rhys Campbell <rhys.james.campbell@googlemail.com>
2020-06-17 12:59:06 +0200

    Refactor code in remaining NodeTool3PairCommand modules

--- plugins/module_utils/NodeToolCmdObjects.py
+++ plugins/module_utils/NodeToolCmdObjects.py
@@ -2,6 +2,7 @@ from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 import socket
 
+
 class NodeToolCmd(object):
     """
     This is a generic NodeToolCmd class for building nodetool commands
