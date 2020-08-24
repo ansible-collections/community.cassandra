2656688cd35eebd428913c7244e48e99e9329ac8
Rhys <rhys.james.campbell@googlemail.com>
2020-08-24 17:00:17 +0200

    Role tidy (#70)
    * Remove role
    
    * Update CI
    
    * Remove cassandra_config role files from ignores
    
    * Add sh==1.13.1

--- travis_run.sh
+++ travis_run.sh
@@ -19,18 +19,28 @@ test_count=0;
 
 declare -a role_list=();
 
-for role in $(git diff --name-only HEAD~1 | grep roles/ | cut -d'/' -f -2 | sort | uniq); do
-    if [[ -d "$role/molecule" ]]; then
-      if [[ ! -f "$role/molecule/.travisignore" ]]; then
-        echo "Adding $role to test queue"
-        role_list+=( $role );
+if [ -z "${ISMASTER+x}" ]; then
+  for role in $(git diff --name-only HEAD~1 | grep roles/ | cut -d'/' -f -2 | sort | uniq); do
+      if [[ -d "$role/molecule" ]]; then
+        if [[ ! -f "$role/molecule/.travisignore" ]]; then
+          echo "Adding $role to test queue"
+          role_list+=( $role );
+        else
+          echo "The role $role has been specifically excluded from travis with a .travisignore file";
+        fi;
       else
-        echo "The role $role has been specifically excluded from travis with a .travisignore file";
+          echo "The role $role does not have a molecule sub-directoy so skipping tests."
       fi;
-    else
-        echo "The role $role does not have a molecule sub-directoy so skipping tests."
-    fi;
-done
+  done
+else  # CASSANDRA_ROLE should be defined
+  set +u;
+  if [ -z "$CASSANDRA_ROLE" ]; then
+    echo "CASSANDRA_ROLE was not set as expected.";
+  else
+    role_list+=( "$CASSANDRA_ROLE" );
+  fi;
+  set -u;
+fi;
 
 if [ ${#role_list[@]} -ne 0 ]; then
   for role in "${role_list[@]}"; do
