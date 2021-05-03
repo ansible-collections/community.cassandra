955fe6164db17f8debd496503e80291a62a95134
Rhys Campbell <rhys.james.campbell@googlemail.com>
2021-05-03 11:45:45 +0200

    Minor tweaks

--- plugins/modules/cassandra_cqlsh.py
+++ plugins/modules/cassandra_cqlsh.py
@@ -330,7 +330,7 @@ def main():
         result['cmd'] = cmd
 
     if rc != 0:
-        module.fail_json(**result)
+        module.fail_json(msg="module execution failed", **result)
     else:
         result['changed'] = True
         try:
