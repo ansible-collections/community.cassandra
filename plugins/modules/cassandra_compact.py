3583086b72d4db17875b27982b09b16f5f183cc9
Rhys Campbell <rhys.james.campbell@googlemail.com>
2021-11-21 12:25:16 +0100

    Initial commit pof module

---
+++ plugins/modules/cassandra_compact.py
@@ -0,0 +1,138 @@
+#!/usr/bin/python
+
+# 2021 Rhys Campbell rhyscampbell@bluewin.ch
+# https://github.com/rhysmeister
+# GNU General Public License v3.0+
+# (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+from __future__ import absolute_import, division, print_function
+
+
+DOCUMENTATION = '''
+---
+module: cassandra_compact
+author: Rhys Campbell (@rhysmeister)
+short_description: Manage compaction on the Cassandra node.
+requirements:
+  - nodetool
+description:
+    - Manage compaction on the Cassandra node.
+
+extends_documentation_fragment:
+  - community.cassandra.nodetool_module_options
+
+options:
+  compact:
+    description:
+      - The required status
+    type: bool
+    default: true
+    required: true
+'''
+
+EXAMPLES = '''
+- name: RUu compaction on the node
+  community.cassandra.cassandra_compact:
+    compaction: yes
+
+- name: Stop compaction on the node
+  community.cassandra.cassandra_compact:
+    compaction: no
+'''
+
+RETURN = '''
+cassandra_compact:
+  description: The return state of the executed command.
+  returned: success
+  type: str
+'''
+
+from ansible.module_utils.basic import AnsibleModule, load_platform_subclass
+import socket
+__metaclass__ = type
+
+
+from ansible_collections.community.cassandra.plugins.module_utils.nodetool_cmd_objects import NodeToolCmd, NodeTool3PairCommand
+from ansible_collections.community.cassandra.plugins.module_utils.cassandra_common_options import cassandra_common_argument_spec
+
+
+def main():
+    argument_spec = cassandra_common_argument_spec()
+    argument_spec.update(
+        compact=dict(required=True, default=True, type='bool')
+    )
+    module = AnsibleModule(
+        argument_spec=argument_spec,
+        supports_check_mode=True,
+    )
+
+    status_cmd = 'compactionstats'
+    enable_cmd = 'compact'
+    disable_cmd = 'stop'
+    status_inactive = 'pending tasks: 0'
+
+    n = NodeTool3PairCommand(module, status_cmd, enable_cmd, disable_cmd)
+
+    rc = None
+    out = ''
+    err = ''
+    result = {}
+
+    (rc, out, err) = n.status_command()
+    out = out.strip()
+    if module.params['debug']:
+        if out:
+            result['stdout'] = out
+        if err:
+            result['stderr'] = err
+
+    if module.params['compact'] is False:
+
+        if rc != 0:
+            module.fail_json(name=status_cmd,
+                             msg="status command failed", **result)
+        if module.check_mode:
+            if out != status_inactive:
+                module.exit_json(changed=True, msg="check mode", **result)
+            else:
+                module.exit_json(changed=False, msg="check mode", **result)
+        if out != status_inactive:
+            (rc, out, err) = n.disable_command()
+            if module.params['debug']:
+                if out:
+                    result['stdout'] = out
+                if err:
+                    result['stderr'] = err
+        if rc != 0:
+            module.fail_json(name=disable_cmd,
+                             msg="stop command failed", **result)
+        else:
+            result['changed'] = True
+
+    elif module.params['compact'] is True:
+
+        if rc != 0:
+            module.fail_json(name=status_cmd,
+                             msg="compactionstats command failed", **result)
+        if module.check_mode:
+            if out == status_inactive:
+                module.exit_json(changed=True, msg="check mode", **result)
+            else:
+                module.exit_json(changed=False, msg="check mode", **result)
+        if out == status_inactive:
+            (rc, out, err) = n.enable_command()
+            if module.params['debug']:
+                if out:
+                    result['stdout'] = out
+                if err:
+                    result['stderr'] = err
+        if rc is not None and rc != 0:
+            module.fail_json(name=enable_cmd,
+                             msg="compact command failed", **result)
+        else:
+            result['changed'] = True
+
+    module.exit_json(**result)
+
+
+if __name__ == '__main__':
+    main()
