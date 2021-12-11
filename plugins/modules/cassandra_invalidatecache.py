a5dcbdac29614764fb854fb4e528b7c3168cdadd
Rhys Campbell <rhys.james.campbell@googlemail.com>
2021-12-11 19:18:39 +0100

    Add idempotency

--- plugins/modules/cassandra_invalidatecache.py
+++ plugins/modules/cassandra_invalidatecache.py
@@ -17,7 +17,6 @@ requirements:
 description:
   - Invalidates the various caches on the Cassandra node.
   - Invalidates the Counter, Key and Row caches.
-  - This module is not idempotent.
 
 extends_documentation_fragment:
   - community.cassandra.nodetool_module_options
@@ -63,12 +62,54 @@ msg:
 from ansible.module_utils.basic import AnsibleModule, load_platform_subclass
 import socket
 import subprocess
+import re
 __metaclass__ = type
 
 
 from ansible_collections.community.cassandra.plugins.module_utils.nodetool_cmd_objects import NodeToolCmd, NodeToolCommandSimple
 from ansible_collections.community.cassandra.plugins.module_utils.cassandra_common_options import cassandra_common_argument_spec
 
+
+def parse_cache_info(info: str, module):
+    """
+    This function parses the output from the nodetool  info command
+    in order to return the cache info, i.e. 
+    'ID                     : f4ee490c-df8e-4a8d-9236-320903697fbf
+    Gossip active          : true
+    Native Transport active: true
+    Load                   : 145.17 KiB
+    Generation No          : 1638800353
+    Uptime (seconds)       : 225798
+    Heap Memory (MB)       : 258.26 / 495.00
+    Off Heap Memory (MB)   : 0.00
+    Data Center            : datacenter1
+    Rack                   : rack1
+    Exceptions             : 10
+    Key Cache              : entries 10, size 896 bytes, capacity 24 MiB, 48 hits, 62 requests, 0.774 recent hit rate, 14400 save period in seconds
+    Row Cache              : entries 0, size 0 bytes, capacity 0 bytes, 0 hits, 0 requests, NaN recent hit rate, 0 save period in seconds
+    Counter Cache          : entries 0, size 0 bytes, capacity 12 MiB, 0 hits, 0 requests, NaN recent hit rate, 7200 save period in seconds
+    Percent Repaired       : 100.0%
+    Token                  : -6148914691236517206'
+
+    {
+      "key_cache_entries": 10,
+      "row_cache_entries": 0,
+      "counter_cache_entries": 0
+    }
+
+    TODO - This should be placed into common code and unit tested
+    """
+    p = re.compile("Key Cache .*: entries \d+")
+    key_cache_entries = p.search(info).group(0).split()[-1:]
+    p = re.compile("Row Cache .*: entries \d+")
+    row_cache_entries = p.search(info).group(0).split()[-1:]
+    p = re.compile("Counter Cache .*: entries \d+")
+    counter_cache_entries = p.search(info).group(0).split()[-1:]
+    if key_cache_entries is None or row_cache_entries  is None or counter_cache_entries is None:
+        module.fail_json(msg="Unable to get cache info")
+    return {"key_cache_entries": key_cache_entries,
+            "row_cache_entries": row_cache_entries,
+            "counter_cache_entries": counter_cache_entries}
 
 def main():
 
@@ -81,19 +122,12 @@ def main():
     )
     module = AnsibleModule(
         argument_spec=argument_spec,
-        supports_check_mode=False,
+        supports_check_mode=True,
     )
-
-    cache = module.params['cache']
-
-    cmd = 'invalidate{0}cache'.format(cache)
-
-    n = NodeToolCommandSimple(module, cmd)
-
-    rc = None
-    out = ''
-    err = ''
     result = {}
+
+    cmd = "info"
+    n = NodeToolCommandSimple(module, cmd)
 
     (rc, out, err) = n.run_command()
     out = out.strip()
@@ -106,12 +140,49 @@ def main():
             result['stderr'] = err
 
     if rc == 0:
-        result['changed'] = True
-        result['msg'] = "The {0} cache was invalidated".format(cache)
-        module.exit_json(**result)
+
+        cache = module.params['cache']
+        cmd = 'invalidate{0}cache'.format(cache)
+        cache_info = parse_cache_info(out)
+
+        if cache == "key" and cache_info['key'] > 0 \
+          or cache == "row" and cache_info['row'] > 0 \
+              or cache == "counter" and cache_info['counter'] > 0:
+            n = NodeToolCommandSimple(module, cmd)
+
+            rc = None
+            out = ''
+            err = ''
+            result = {}
+
+            if module.check_mode is False:
+                (rc, out, err) = n.run_command()
+            else:
+                rc = 0
+                out = None
+                err = None
+            out = out.strip()
+            err = err.strip()
+
+            if module.params['debug']:
+                if out:
+                    result['stdout'] = out
+                if err:
+                    result['stderr'] = err
+
+            if rc == 0:
+                result['changed'] = True
+                result['msg'] = "The {0} cache was invalidated".format(cache)
+                module.exit_json(**result)
+            else:
+                result['msg'] = "Failed invalidating the {0} cache".format(cache)
+                module.fail_json(**result)
+        else:
+            result['msg'] = "The {0} cache is empty"
+            result['changed'] = False
     else:
-        result['msg'] = "Failed invalidating the {0} cache".format(cache)
-        module.fail_json(**result)
+        result['msg'] = "Failed getting cache info"
+        module.fail_json(**result)    
 
 
 if __name__ == '__main__':
