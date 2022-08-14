ced0670bcb732e69febae70d721128e642772d86
Rhys <rhys.james.campbell@googlemail.com>
2022-08-14 17:58:50 +0200

    Removes a few imports that are not needed. (#228)

--- plugins/modules/cassandra_truncatehints.py
+++ plugins/modules/cassandra_truncatehints.py
@@ -33,12 +33,12 @@ cassandra_flush:
   type: str
 '''
 
-from ansible.module_utils.basic import AnsibleModule, load_platform_subclass
+from ansible.module_utils.basic import AnsibleModule
 import socket
 __metaclass__ = type
 
 
-from ansible_collections.community.cassandra.plugins.module_utils.nodetool_cmd_objects import NodeToolCmd, NodeToolCommandSimple
+from ansible_collections.community.cassandra.plugins.module_utils.nodetool_cmd_objects import NodeToolCommandSimple
 from ansible_collections.community.cassandra.plugins.module_utils.cassandra_common_options import cassandra_common_argument_spec
 
 
