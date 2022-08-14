625c6e1edb1a125bcb6bb3d8cd913f40635bee3b
Rhys Campbell <rhys.james.campbell@googlemail.com>
2022-08-14 12:20:03 +0200

    pep8

--- plugins/modules/cassandra_reload.py
+++ plugins/modules/cassandra_reload.py
@@ -64,7 +64,7 @@ import socket
 __metaclass__ = type
 
 
-from ansible_collections.community.cassandra.plugins.module_utils.nodetool_cmd_objects import  NodeToolCommandSimple
+from ansible_collections.community.cassandra.plugins.module_utils.nodetool_cmd_objects import NodeToolCommandSimple
 from ansible_collections.community.cassandra.plugins.module_utils.cassandra_common_options import cassandra_common_argument_spec
 
 
