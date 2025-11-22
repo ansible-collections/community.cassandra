1046b2943d8f1eceb908061424f09952ce5c4dec
Rhys Campbell <rhys.james.campbell@googlemail.com>
2025-11-22 18:09:25 +0100

    Update test

--- roles/cassandra_firewall/molecule/default/tests/test_default.py
+++ roles/cassandra_firewall/molecule/default/tests/test_default.py
@@ -19,39 +19,16 @@ def test_ensure_firewalld_commands(host):
         assert cmd.rc == 0
 
 
-def test_ensure_cassandra_ports_open(host):
-    expected_ports = [
-        '22/tcp',
-        '7000/tcp',
-        '7001/tcp',
-        '7199/tcp',
-        '9042/tcp',
-        '9160/tcp'
-    ]
-
-    with host.sudo():
-        # Detect firewall
-        if host.exists("firewall-cmd"):
+def test_ensure_cassandra_ports_open_redhat(host):
+    if host.system_info.distribution == "redhat":
+        expected_output = ['22/tcp',
+                        '7000/tcp',
+                        '7001/tcp',
+                        '7199/tcp',
+                        '9042/tcp',
+                        '9160/tcp']
+        with host.sudo():
             cmd = host.run("firewall-cmd --list-ports")
-            raw_ports = cmd.stdout.strip().split()
-            
-            # firewalld outputs exactly "7000/tcp"
-            opened_ports = sorted(raw_ports)
 
-        elif host.exists("ufw"):
-            cmd = host.run("ufw status")
-            opened_ports = []
-
-            for line in cmd.stdout.splitlines():
-                # Match lines like: "9042/tcp                  ALLOW       Anywhere"
-                parts = line.split()
-                if len(parts) > 0 and "/" in parts[0]:
-                    opened_ports.append(parts[0].strip())
-
-            opened_ports = sorted(opened_ports)
-
-        else:
-            raise AssertionError("No supported firewall found (firewalld or ufw).")
-
-        assert opened_ports == expected_ports
-
+            # Output is not always in the same order so we need to order it ourselves
+            assert sorted(cmd.stdout.strip().split(" ")) == expected_output
