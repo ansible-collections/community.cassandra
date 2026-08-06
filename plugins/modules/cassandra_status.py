b060262b1258d7e3c9e4a938d13b032bb33a9a01
Alain Rodriguez <arodrime@gmail.com>
2026-08-06 12:55:15 +0200

    Declare cassandra_status as supporting check mode (read-only module)

--- plugins/modules/cassandra_status.py
+++ plugins/modules/cassandra_status.py
@@ -312,7 +312,7 @@ def main():
     )
     module = AnsibleModule(
         argument_spec=argument_spec,
-        supports_check_mode=False,
+        supports_check_mode=True,
     )
 
     down = module.params['down']
